f62473a736e25100fde82b404626c03d5d90780a
Jose <jmonagas@hotmail.ca>
2020-09-29 21:17:48 -0600

    It was a great session tonight. Together, the group and the instructor worked on creating the login form, login page, registration, cookies, etc. What I appreciate the most is the fact that we all were able to experience the complexities of a project like this, where even our instructor had issues to make everything work. This is the type of didactic approach that should be implemented at InnoTech College. An atmosphere that is fully focused on learning the ropes. It is not that we (the students) are not fit for the tasks, but that we just need to be guided through the process before we can actually walk the mile alone. That is the whole point of adult education.

---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,81 @@
+<template>
+  <div id="firstpage">
+    <p>Email</p>
+    <input type="text" id="email-input" v-model="email" />
+
+    <p>Username</p>
+    <input type="text" id="username-input" v-model="username" />
+
+    <p>Password</p>
+    <input type="text" id="password-input" v-model="password" />
+
+    <p>Bio</p>
+    <textarea id="bio-input" v-model="bio"></textarea>
+
+    <p>Birthdate (YYYY-MM-DD)</p>
+    <input type="text" id="birthdate-input" v-model="birthdate" />
+    <br />
+    <br />
+    <button @click="signupUser">Sign Up</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import cookies from "vue-cookies";
+
+export default {
+  name: "signup-form",
+  data() {
+    return {
+      email: "",
+      username: "",
+      password: "",
+      bio: "",
+      birthdate: ""
+    };
+  },
+  methods: {
+    signupUser: function() {
+      axios
+        .request({
+          method: "POST",
+          url: "https://tweeterest.ml/api/users",
+          headers: {
+            "Content-Type": "application/json",
+            "X-Api-Key": "KuxH91zPs9WUhLLav8O6KNil0o4lB1vXKPYnN1nLbJTEl"
+          },
+          data: {
+            email: this.email,
+            password: this.password,
+            username: this.username,
+            bio: this.bio,
+            birthdate: this.birthdate
+          }
+        })
+        .then(response => {
+          // Write Code for Validation of Cookie
+          cookies.set("session", response.data[0].loginToken);
+          console.log(response);
+          //Create Function to Send to Home
+        })
+        .catch(error => {
+          console.log(error);
+        });
+    }
+  }
+};
+</script>
+
+<style scoped>
+#firstpage {
+  background-color: rgb(189, 233, 247);
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  border-radius: 20px;
+}
+#bio-input {
+  width: 250px;
+  height: 50px;
+}
+</style>
---
+++ src/views/SignUpPage.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <router-link to="/login">Login</router-link>
+    <h3>Please Register</h3>
+    <sign-up-form></sign-up-form>
+  </div>
+</template>
+
+<script>
+import SignUpForm from "../components/SignUpForm.vue";
+
+export default {
+  name: "signup-page",
+  components: {
+    SignUpForm
+  }
+};
+</script>
+
+<style scoped>
+</style>
